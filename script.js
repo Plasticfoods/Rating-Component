edfc607181cee600461069e546153482dea077b2
Rajdeep Karmakar <rajdeep.hasCode@gmail,com>
2023-02-08 18:26:35 +0530

    thank you page, mobile completion

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 
 const ratingButtons = document.querySelectorAll('.rating');
+const submitButton = document.querySelector('.btn-submit');
+const ratingBox = document.querySelector('.rating-box');
+const thankyouBox = document.querySelector('.thank-you-box');
+let userRating;
 
-ratingButtons.forEach(function(button) {
-    button.addEventListener('click', () => {
+// Rating buttons click event
+ratingButtons.forEach(button => {
+
+    button.addEventListener('click', function ratingButtonClick() {
         resetRatingButtons();
         button.style.backgroundColor = 'hsl(217, 12%, 63%)';
         button.childNodes[0].style.color = 'hsl(0, 0%, 100%)';
+        userRating = this.getAttribute('value');
     });
 });
 
+
+// Reset rating buttons after click
 function resetRatingButtons() {
-    ratingButtons.forEach((button) => {
+    ratingButtons.forEach(button => {
         button.style.backgroundColor = 'hsl(214, 12%, 23%)';
         button.childNodes[0].style.color = 'hsl(216, 12%, 54%)';
     });
 }
+
+// submit button click
+submitButton.addEventListener('click', function() {
+    ratingBox.style.display = 'none';
+    thankyouBox.style.display = 'block';
+
+    const resultContent = document.querySelector('.result-content');
+    console.log(userRating);
+    console.log(resultContent);
+    resultContent.innerHTML = `You selected ${userRating} out of 5`;
+})
+
